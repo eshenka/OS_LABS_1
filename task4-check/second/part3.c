a15dad20e2e60893782e6de305ba5ad529168580
eshenka <yuliaolegyureva@gmail.com>
2024-05-21 14:24:41 +0700

    signal handling

--- task4-check/second/part3.c
+++ task4-check/second/part3.c
@@ -1,8 +1,18 @@
+#include <bits/types/sigset_t.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <signal.h>
+#include <memory.h>
+#include <string.h>
+
+void handler(int sig) {
+    printf("Got a signal %d\n", sig);
+    exit(0);
+}
 
 void heap_allocation(int N, int iters) {
     char* buffers[iters];
@@ -11,18 +21,18 @@ void heap_allocation(int N, int iters) {
 
     for (int i = 0; i < iters; i++) {
         buffers[i] = (char*) malloc(N * sizeof(char));
-        sleep(1);
+        //sleep(1);
     }
 
     printf("Allocation ends\n");
 
-    sleep(5);
+    //sleep(5);
 
     printf("Freeing begins\n");
     
     for (int i = 0; i < iters; i++) {
         free(buffers[i]);
-        sleep(1);
+        //sleep(1);
     }
 
     printf("Freeing ends\n");
@@ -39,7 +49,7 @@ void add_pages(int pages_number) {
     }
 
     printf("Added pages\n");
-    sleep(5);
+    //sleep(5);
 
     int protect = mprotect(map, page_size, PROT_WRITE);
     if (protect == -1) {
@@ -48,7 +58,7 @@ void add_pages(int pages_number) {
     }
 
     printf("Changed mode\n");
-    sleep(5);
+    //sleep(5);
 
     //OK
     *map = 1;
@@ -63,10 +73,13 @@ void add_pages(int pages_number) {
     }
 
     printf("Changed mode\n");
-    sleep(5);
+    //sleep(5);
+
 
     //SEGFAULT
-    //*map = 1;
+    
+    if (signal(SIGSEGV, handler) == SIG_ERR) printf("caught\n");
+    *map = 1;
 
     //OK
     printf("%d\n", map[0]);
@@ -79,20 +92,20 @@ void add_pages(int pages_number) {
 
     printf("Unmapped pages\n");
 
-    sleep(5);
+    //sleep(5);
 } 
 
 int main() {
     pid_t pid = getpid();
     printf("PID: %d\n", pid);
 
-    sleep(10);
+    //sleep(10);
 
     int iterations = 20;
     int size = 204800;
     heap_allocation(size, iterations);
 
-    sleep(5);
+    //sleep(5);
 
     add_pages(10);
 
