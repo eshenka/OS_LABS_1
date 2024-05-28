58abebbc39fdbd7addf8eb0d85101004ff5068cb
eshenka <yuliaolegyureva@gmail.com>
2024-05-28 23:09:58 +0700

    added udp server

--- task7-check/first/udp-server.c
+++ task7-check/first/udp-server.c
@@ -3,12 +3,16 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <netinet/ip.h>
+#include <string.h>
 
 #define PORT 10000
 #define BUFFER_SIZE 256
 
 int main() {
-    int socket_fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
 
     if (socket_fd == -1) {
         perror("Unable to create socket\n");
@@ -16,10 +20,27 @@ int main() {
     }
 
     struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
 
     memset(&server_addr, 0, sizeof(server_addr));
-    memset(&client_addr, 0, sizeof(client_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = PORT;
+    server_addr.sin_addr.s_addr = INADDR_LOOPBACK;
 
+    int bind_status = bind(socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (bind_status != 0) {
+        perror("Unable to bind");
+        exit(-1);
+    }
 
+    while(1) {
+        char buffer[BUFFER_SIZE];
+
+        struct sockaddr_in client_socket;
+
+        ssize_t recieved = recvfrom(socket_fd, &buffer, BUFFER_SIZE, 0, (struct sockaddr *) &client_socket, NULL);
+
+        printf("%s\n", buffer);
+
+        sendto(socket_fd, buffer, recieved, 0, (struct sockaddr *) &client_socket, NULL);
+    }
 }
