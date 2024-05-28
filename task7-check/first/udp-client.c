9843caf759d42a90879161ba77346a6c795fbcd8
eshenka <yuliaolegyureva@gmail.com>
2024-05-29 01:58:13 +0700

    udp

--- task7-check/first/udp-client.c
+++ task7-check/first/udp-client.c
@@ -23,36 +23,37 @@ int main() {
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-
-    int bind_status = bind(socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr));
-    if (bind_status != 0) {
-        perror("Unable to bind");
-        exit(-1);
-    }
-
+    
     while(1) {
         char buffer[BUFFER_SIZE];
 
-        struct sockaddr client_socket;
-
-        socklen_t addrlen;
-    
-        ssize_t recieved = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &client_socket, &addrlen);
-
-        if (recieved == -1) {
-            perror("Unable to recieve");
-            continue;
-            //exit(-1);
+        char* helpme = fgets(buffer, BUFFER_SIZE, stdin);
+        if (helpme == NULL) {
+            perror("ya riday");
+            exit(-1);
         }
 
-        write(STDOUT_FILENO, buffer, recieved);
+        struct sockaddr_in client_socket;
+        client_socket.sin_family = AF_INET;
 
-        ssize_t sent = sendto(socket_fd, buffer, recieved, 0, (struct sockaddr *) &client_socket, addrlen);
+        int sent = sendto(socket_fd, buffer, strlen(buffer), 0, (struct sockaddr *) &server_addr, sizeof(server_addr));
 
         if (sent == -1) {
-            perror("Unable to send\n");
-            //exit(-1);
+            printf("Unable to send\n");
+            continue;
         }
+
+        socklen_t addrlen;
+
+        ssize_t recieved = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &server_addr, &addrlen);
+
+        if (recieved == -1) {
+            printf("Unable to recieve\n");
+            break;
+        }
+
+        printf("%s\n", buffer);
+
     }
 
     close(socket_fd);
