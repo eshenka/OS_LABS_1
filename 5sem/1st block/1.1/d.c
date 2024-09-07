fa362260935d09e55e5cd0a442a455abab3021c9
eshenka <yuliaolegyureva@gmail.com>
2024-09-08 01:26:59 +0700

    more tasks

--- 5sem/1st block/1.1/d.c
+++ 5sem/1st block/1.1/d.c
@@ -5,21 +5,35 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <time.h>
 
 int GLOBAL_VALUE = 123456;
 
-void *mythread(void *arg) {
-    printf("mythread: gettid() = %d\n", gettid());
-    printf("mythread: pthread_self() = %lu\n", pthread_self());
+typedef struct Args {
+    int local_value;
+} Args;
 
-    static int static_thread = 125478;
-    const int const_thread = 123654;
-    int local_thread = 521478;
+void *mythread(void *args) {
+    pid_t pid = getpid();
+
+    printf("\n\nPID = %d\n\n", pid);
+
+    Args* values = args;
+    
+    static int static_thread = 1245;
+    const int const_thread = 147855;
+    int local_thread = 477744;
 
     printf("thread_static = %p\n", &static_thread);
     printf("thread_const = %p\n", &const_thread);
     printf("thred_local = %p\n", &local_thread);
     printf("thread_global = %p\n", &GLOBAL_VALUE);
+    printf("thread_local_value_pointer = %p\n", &values->local_value);
+
+    sleep(3);
+
+    values->local_value = 0;
+    GLOBAL_VALUE = 0;
 
 	return NULL;
 }
@@ -28,15 +42,17 @@ int main() {
 	pthread_t tid;
 	int err;
 
-	printf("main [pid = %d ppid = %d ttid = %d]\n", getpid(), getppid(), gettid());
+    Args args;
+    args.local_value = 147852;
 
-	err = pthread_create(&tid, NULL, mythread, NULL);
+	err = pthread_create(&tid, NULL, mythread, &args);
 	if (err) {
 	    printf("main: pthread_create() failed: %s\n", strerror(err));
 		return -1;
 	}
 
-    printf("main: pthread_create tid = %lu\n", tid);
+    printf("\n\nCHILD TID = %lu\n\n", tid);
+    printf("\n\nMAIN TID = %d\n\n", gettid());
 
     static int static_main = 147852;
     const int const_main = 523986;
@@ -47,11 +63,26 @@ int main() {
     printf("main_local = %p\n", &local_main);
     printf("main_global = %p\n", &GLOBAL_VALUE);
 
+
+
+    printf("global_unchaged = %d\n", GLOBAL_VALUE);
+    printf("local_unchaged = %d\n", args.local_value);
+    printf("local_value_pointer = %p\n", &args.local_value);
+    sleep(5);
+    printf("global_changed = %d\n", GLOBAL_VALUE);
+    printf("local_changed = %d\n", args.local_value);
+
+
     err = pthread_join(tid, NULL);
     if (err != 0) {
         perror("Failed");
         return -1;
     }
 
+    sleep(60);
+
 	return 0;
 }
+
+
+
