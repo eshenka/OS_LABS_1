cb610111cc8596ae0af5950c846c211906bf7622
eshenka <yuliaolegyureva@gmail.com>
2024-09-11 22:38:05 +0700

    ok

---
+++ 5sem/1st block/1.3/a.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+typedef struct Mystruct {
+    int integer;
+    char* str;
+} Mystruct;
+
+void* print_struct(void* args) {
+    int err = pthread_detach(pthread_self());
+    if (err != 0) {
+        perror("Failed detach");
+        pthread_exit(NULL);
+    }
+
+    Mystruct* object = (Mystruct*) args;
+
+    printf("integer = %d\nstring = %s\n", object->integer, object->str);
+
+    pthread_exit(NULL);
+}
+
+int main() {
+    Mystruct object = {
+        .integer = 123486,
+        .str = "hello world",
+    };
+
+    pthread_t tid;
+    int err;
+
+    err = pthread_create(&tid, NULL, print_struct, &object);
+    if (err != 0) {
+        perror("Failed create");
+        return -1;
+    }
+
+    // err = pthread_join(tid, NULL);
+    // if (err != 0) {
+    //     perror("Failed join");
+    //     return -2;
+    // }
+
+}
