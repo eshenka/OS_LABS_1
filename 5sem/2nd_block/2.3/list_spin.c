0d91076869359daeb837c8017fc57d6d91428564
eshenka <yuliaolegyureva@gmail.com>
2024-11-13 22:20:59 +0700

    implement spinlock for list

---
+++ 5sem/2nd_block/2.3/list_spin.c
@@ -0,0 +1,15 @@
+#include <pthread.h>
+
+#include "linked_list.h"
+
+struct sync {
+    pthread_spinlock_t spin;
+};
+
+void lock_init(Node* node) { pthread_spin_init(&node->lock->spin, 0); }
+
+void read_lock(Node* node) { pthread_spin_lock(&node->lock->spin); }
+
+void write_lock(Node* node) { pthread_spin_lock(&node->lock->spin); }
+
+void unlock(Node* node) { pthread_spin_unlock(&node->lock->spin); }
